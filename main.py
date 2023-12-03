c6a087b0e801a3fca7ea0d3e47179f3cd3751bb7
Maciej Bąba <maciek2baba2@gmail.com>
2023-12-03 22:50:08 +0100

    change main, add image

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+print("Initializing...")
+print("Importing modules...")
+
+print("Importing cv2...")
 import cv2
+
+print("Importing numpy...")
 import numpy as np
+
+print("Importing os...")
 import os
+
+print("Importing matplotlib...")
 import matplotlib.pyplot as plt
+
+print("Importing tensorflow...")
 import tensorflow as tf
 
+print("Modules imported successfully")
 
-mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# # uncomment this to train the model
 
-# uncomment this to train the model
+# print("Declaring mnist...")
+# mnist = tf.keras.datasets.mnist
+
+# print("Loading mnist data...")
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # x_train = tf.keras.utils.normalize(x_train, axis=1)
 # x_test = tf.keras.utils.normalize(x_test, axis=1)
@@ -27,9 +43,38 @@ mnist = tf.keras.datasets.mnist
 # model.save('digits.model')
 
 
+# uncomment this to test the model
+
+# model = tf.keras.models.load_model("digits.model")
+
+# loss, accuracy = model.evaluate(x_test, y_test)
+
+# print(loss)
+# print(accuracy)
+
+
+print("Loading model...")
 model = tf.keras.models.load_model("digits.model")
 
-loss, accuracy = model.evaluate(x_test, y_test)
+print("Declaring image_number...")
+image_number = 1
 
-print(loss)
-print(accuracy)
+print("Starting loop...")
+while os.path.isfile(f"images/{image_number}.png"):
+    print("Trying to read image...")
+    try:
+        print(f"Reading image {image_number}...")
+        img = cv2.imread(f"images/{image_number}.png")[:, :, 0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"The number is probably a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error, something went wrong")
+    finally:
+        print("Incrementing image_number...")
+        image_number += 1
+
+print("Done")
+print("Exiting...")
