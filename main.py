2b09ca5579d7144ee7206dcd6c51a6224b9a511e
Maciej Bąba <maciek2baba2@gmail.com>
2023-12-03 22:13:45 +0100

    create and train model

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-mnist = tf.keras.datasets.mnist
+# uncomment this to train the model
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# mnist = tf.keras.datasets.mnist
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=3)
+
+# model.save('digits.model')
+
+
+model = tf.keras.models.load_model('digits.model')
 
